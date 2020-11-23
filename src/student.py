d40bcebd0faf418c0bb416aebbc7112c2224086f
evelyn-sz <evelynszarek@gmail.com>
2020-11-23 13:58:53 +0000

    Test 1 set_up_student

--- src/student.py
+++ src/student.py
@@ -1,1 +1,7 @@
-
+class Student:
+    def __init__(self, name, cohort):
+        self.name = name,
+        self.cohort = cohort
+        
+student_1 = Student("Ada", "E42")
+print(student_1.name, student_1.cohort)
